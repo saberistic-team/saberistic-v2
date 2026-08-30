3d5ea1ef0e660fb53a8bb9a296183b4e8a970a40
AmirSaber Sharifi <inbox@saberistic.com>
2026-08-30 13:53:10 -0400

    fix: type WebP static assets in clean builds

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -2,3 +2,8 @@ declare module '*.png' {
   const image: import('next/image').StaticImageData
   export default image
 }
+
+declare module '*.webp' {
+  const image: import('next/image').StaticImageData
+  export default image
+}
